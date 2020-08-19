aaf8b0d93e5eac8cf11c99bdaef4256b98336752
Axel Espinosa <axeldavid45@gmail.com>
2020-08-19 11:39:04 -0500

    Test the stub methods with sinon

--- platziverse-db/test/agent-test.js
+++ platziverse-db/test/agent-test.js
@@ -17,28 +17,43 @@ const config = {
 }
 
 // Creating stubs to use instead the original objects
-let MetricStub = {
-  belongsTo: function () {}
+const MetricStub = {
+  belongsTo: sinon.spy()
 }
 
 // Create the stubs for the AgentModel, every time we run a new test we need a new stub
 let AgentStub = null
-
-beforeEach(async function () {
-  AgentStub = {
-    hasMany: function () {}
-  }
-
-  const setupDatabase = proxyquire('../', {
-    './models/agent': () => AgentStub,
-    './models/metric': () => MetricStub
+let sandBox = null
+describe('Agent service', function () {
+  beforeEach(async function () {
+    sandBox = sinon.createSandbox()
+    AgentStub = {
+      hasMany: sandBox.spy()
+    }
+    // Replace the original models with the stubs
+    const setupDatabase = proxyquire('../', {
+      './models/agent': () => AgentStub,
+      './models/metric': () => MetricStub
+    })
+    db = await setupDatabase(config)
   })
 
-  db = await setupDatabase(config)
-})
+  afterEach(function () {
+    // Restores the status of the stubs
+    sandBox.restore()
+  })
 
-describe('Agent service', function () {
   it('should exist agent in the api', function () {
     expect(db).to.have.a.property('Agent')
+  })
+  it('should call the stub methods', function () {
+    expect(AgentStub.hasMany.called).to.be.equal(true)
+    expect(MetricStub.belongsTo.called).to.be.equal(true)
+
+  })
+
+  it('should call the stub method with the stub objects', function () {
+    expect(MetricStub.belongsTo.calledWith(AgentStub)).to.be.equal(true)
+    expect(AgentStub.hasMany.calledWith(MetricStub)).to.be.equal(true)
   })
 })
