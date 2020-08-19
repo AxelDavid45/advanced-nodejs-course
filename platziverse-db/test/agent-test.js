b72c47b86845fdf26bac2d627949b5656289b7f7
Axel Espinosa <axeldavid45@gmail.com>
2020-08-19 11:24:41 -0500

    Added Eslint plugin to mocha, created a test with mocha

--- platziverse-db/test/agent-test.js
+++ platziverse-db/test/agent-test.js
@@ -1,5 +1,7 @@
 'use strict'
 const { expect } = require('chai')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 
 let db = null
 const config = {
@@ -14,8 +16,24 @@ const config = {
   }
 }
 
-beforeEach(async () => {
-  const setupDatabase = require('../')
+// Creating stubs to use instead the original objects
+let MetricStub = {
+  belongsTo: function () {}
+}
+
+// Create the stubs for the AgentModel, every time we run a new test we need a new stub
+let AgentStub = null
+
+beforeEach(async function () {
+  AgentStub = {
+    hasMany: function () {}
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
 })
 
