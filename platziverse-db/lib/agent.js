994048fce37af25dbb05c54391f9d29b5bf06b1a
Axel Espinosa <axeldavid45@gmail.com>
2020-08-19 20:28:36 -0500

    tests for AgentService#createOrUpdate method

--- platziverse-db/lib/agent.js
+++ platziverse-db/lib/agent.js
@@ -1,10 +1,29 @@
 'use strict'
 module.exports = function setupAgent (AgentModel) {
+  async function createOrUpdate (agent) {
+    const filter = {
+      where: {
+        uuid: agent.uuid
+      }
+    }
+
+    const existsAgent = AgentModel.findOne(filter)
+
+    if (existsAgent) {
+      const updated = AgentModel.update(agent, filter)
+      return updated ? AgentModel.findOne(filter) : existsAgent
+    }
+
+    const result = AgentModel.create(agent)
+    return result.toJSON()
+  }
+
   function filterById (id) {
     return AgentModel.findById(id)
   }
 
   return {
-    filterById
+    filterById,
+    createOrUpdate
   }
 }
--- platziverse-db/test/agent-test.js
+++ platziverse-db/test/agent-test.js
@@ -25,26 +25,43 @@ const MetricStub = {
 }
 
 // Create the stubs for the AgentModel, every time we run a new test we need a new stub
-let AgentStub = null
+let AgentModelStub = null
 let sandBox = null
 
 // Create a copy of the single fixture
 const single = Object.assign({}, agentFixtures.single)
+// Create the uuidArguments to filter
+const uuid = 'yyy-yyy-yyy'
+const uuidFilter = {
+  where: {
+    uuid
+  }
+}
 
 describe('Agent service', function () {
   beforeEach(async function () {
     sandBox = sinon.createSandbox()
+
     // Create the stub of the model
-    AgentStub = {
-      hasMany: sandBox.spy(),
+    AgentModelStub = {
+      hasMany: sandBox.spy()
     }
-    // Set up the function to filter by id
-    AgentStub.findById = sandBox.stub()
-    AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+    // Set up the function in the model filterById
+    AgentModelStub.findById = sandBox.stub()
+    AgentModelStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+    // Create the stub in the model for the function findOne
+    AgentModelStub.findOne = sandBox.stub()
+    AgentModelStub.findOne.withArgs(uuidFilter).returns(Promise.resolve(single))
+
+    // Create the update stub function in the model
+    AgentModelStub.update = sandBox.stub()
+    AgentModelStub.update.withArgs(single, uuidFilter).returns(Promise.resolve(single))
 
     // Replace the original models with the stubs
     const setupDatabase = proxyquire('../', {
-      './models/agent': () => AgentStub,
+      './models/agent': () => AgentModelStub,
       './models/metric': () => MetricStub
     })
     db = await setupDatabase(config)
@@ -55,22 +72,37 @@ describe('Agent service', function () {
     sandBox.restore()
   })
 
-  it('should exist agent in the api', function () {
+  it('Should return the AgentService', function () {
     expect(db).to.have.a.property('Agent')
   })
 
   it('should call the stub methods', function () {
-    expect(AgentStub.hasMany.called).to.be.equal(true)
-    expect(MetricStub.belongsTo.called).to.be.equal(true)
-  })
-
-  it('should call the stub method with the stub objects', function () {
-    expect(MetricStub.belongsTo.calledWith(AgentStub)).to.be.equal(true)
-    expect(AgentStub.hasMany.calledWith(MetricStub)).to.be.equal(true)
+    expect(AgentModelStub.hasMany.called).to.be.equal(true, 'Agent has many was executed')
+    expect(MetricStub.belongsTo.called).to.be.equal(true, 'Metric belongs to was executed')
+    expect(MetricStub.belongsTo.calledWith(AgentModelStub)).to.be
+      .equal(true, 'Argument should be AgentModel')
+    expect(AgentModelStub.hasMany.calledWith(MetricStub)).to.be
+      .equal(true, 'Argument should be MetricModel')
   })
 
   it('Agent#FilterById should return the same data', async function () {
     const agent = await db.Agent.filterById(id)
+    expect(AgentModelStub.findById.called).equal(true, 'Should  call the method in the model')
+    expect(AgentModelStub.findById.calledOnce).equal(true, 'Should only call once the method')
+    expect(AgentModelStub.findById.calledWith(id))
+      .equal(true, 'It should call the method with the custom id')
     expect(agent).to.be.deep.equal(agentFixtures.byId(id))
+  })
+
+  it('Agent#CreateOrUpdate method - exists', async function () {
+    const create = await db.Agent.createOrUpdate(single)
+    expect(AgentModelStub.findOne.calledTwice).equal(true, 'Should call the function twice')
+    expect(AgentModelStub.findOne.calledWith(uuidFilter))
+      .equal(true, 'Should call the function with the uuid args')
+    expect(AgentModelStub.update.calledOnce).equal(true, 'Should call the function once')
+    expect(AgentModelStub.update.calledWith(single))
+      .equal(true, 'Should call the function with the single agent fixture')
+
+    expect(create).deep.equal(single, 'Should be the same data')
   })
 })
