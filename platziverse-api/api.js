6a9a647063d9817bc751a2a14fc365b22203fe91
Axel Espinosa <axeldavid45@gmail.com>
2020-08-24 13:33:09 -0500

    Added intersection middleware

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -1,12 +1,38 @@
 'use strict'
 const debug = require('debug')('platziverse:routes:api')
 const errorResponse = require('./utils/error-response')
+const config = require('../config/index')(false, debug)
 const express = require('express')
+const db = require('platziverse-db')
 const api = express.Router()
 
-api.get('/agents', (req, res) => {
-  debug('Request to /agents')
-  res.send({})
+let services = null
+let Agent = null
+let Metric = null
+
+// intersection middleware
+api.use(async (req, res, next) => {
+  if (!services) {
+    debug('Connecting to database')
+    try {
+      services = await db(config)
+      Agent = services.Agent
+      Metric = services.Metric
+      next()
+    } catch (e) {
+      return next(e)
+    }
+  }
+  next()
+})
+
+api.get('/agents', (req, res, next) => {
+  try {
+    debug('Request to /agents')
+    res.send({})
+  } catch (e) {
+    next(e)
+  }
 })
 
 api.get('/agent/:uuid', (req, res, next) => {
